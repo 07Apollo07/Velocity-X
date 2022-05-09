da795f1118ba88dd01d70a88e1628e7431ed59ab
Orvil D'silva <orvildsilva@gmail.com>
2022-05-09 18:50:25 +0530

    Files Read Functions added

---
+++ lib/services/filesDb.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FilesDb{
+  final CollectionReference userCollection = FirebaseFirestore.instance.collection("files");
+
+
+  Future<List> getAllFiles() async{
+
+    QuerySnapshot querySnapshot =await userCollection.get() as QuerySnapshot<Object?>;
+    final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+
+    return allData;
+  }
+
+  Future<List> getAllFilesOrganizationNoWise(String organizationNo) async{
+
+    QuerySnapshot querySnapshot =await userCollection.where("organization_no", isEqualTo: organizationNo).get() as QuerySnapshot<Object?>;
+    final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+
+    return allData;
+  }
+
+  Future<List> getAllFilesCreatorWise(String creatorUID) async{
+
+    QuerySnapshot querySnapshot =await userCollection.where("creator_uid", isEqualTo: creatorUID).get() as QuerySnapshot<Object?>;
+    final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+
+    return allData;
+  }
+
+
+
+
+}
