4018970df92a722af381e2f43ffe2769146d61f5
Justin Dsouza <70844384+07Apollo07@users.noreply.github.com>
2022-08-26 11:12:55 +0530

    Update meta_data.dart

--- lib/screens/metadata/meta_data.dart
+++ lib/screens/metadata/meta_data.dart
@@ -79,37 +79,37 @@ class MetaDataPage extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        // GestureDetector(
-                        //   onTap: () {
-                        //     Get.toNamed(
-                        //       Routes.QR_CODE,
-                        //       id: Constants.homeId,
-                        //       arguments: File.files_uniqueId,
-                        //       // arguments: filesController.files[index],
-                        //     );
-                        //   },
-                        //   child: Container(
-                        //     width: 200,
-                        //     height: 200,
-                        //     decoration: ShapeDecoration(
-                        //       color: Colors.white,
-                        //       shape: RoundedRectangleBorder(
-                        //         side: BorderSide(
-                        //           color: Colors.blue,
-                        //           width: 15,
-                        //         ),
-                        //         borderRadius: BorderRadius.all(
-                        //           Radius.circular(10),
-                        //         ),
-                        //       ),
-                        //     ),
-                        //     margin: EdgeInsets.all(40),
-                        //     child: PlatformAiBarcodeCreatorWidget(
-                        //       creatorController: controller.creatorController!,
-                        //       initialValue: File.files_uniqueId,
-                        //     ),
-                        //   ),
-                        // ),
+                        GestureDetector(
+                          onTap: () {
+                            Get.toNamed(
+                              Routes.QR_CODE,
+                              id: Constants.homeId,
+                              arguments: File.files_uniqueId,
+                              // arguments: filesController.files[index],
+                            );
+                          },
+                          child: Container(
+                            width: 200,
+                            height: 200,
+                            decoration: ShapeDecoration(
+                              color: Colors.white,
+                              shape: RoundedRectangleBorder(
+                                side: BorderSide(
+                                  color: Colors.blue,
+                                  width: 15,
+                                ),
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(10),
+                                ),
+                              ),
+                            ),
+                            margin: EdgeInsets.all(40),
+                            child: PlatformAiBarcodeCreatorWidget(
+                              creatorController: controller.creatorController!,
+                              initialValue: File.files_uniqueId,
+                            ),
+                          ),
+                        ),
                         SizedBox(
                           height: 15,
                         ),
@@ -329,8 +329,8 @@ class MetaDataPage extends StatelessWidget {
                           height: 50,
                         ),
                         Visibility(
-                          visible: false,
-                          // visible: controller.optionsVisible(),
+                          // visible: false,
+                          visible: controller.optionsVisible(),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                             children: [
